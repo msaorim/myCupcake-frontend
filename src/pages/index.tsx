36ee1cff3a17de714e7e7b9c13ae311f4f914ae4
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-10-06 19:07:58 -0300

    sass and _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
+import styles from '../../styles/Home.module.scss'
+
 export default function Home() {
   return (
-    <div>
-      myCupcake ; )
-    </div>
+    <>
+      <Head>
+        <title>myCupcake - Faça seu login</title>
+      </Head>
+      <div>
+        <h1>myCupcake</h1>
+      </div>
+    </>
   )
 }
