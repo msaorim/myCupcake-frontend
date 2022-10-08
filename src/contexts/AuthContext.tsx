783dc1c8382b5637acbb22391e89df4aade3e9fe
msaorim <marcelosaorim@sisconsultoria.com.br>
2022-10-08 15:08:10 -0300

    api configuration

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,14 @@
 import { createContext, ReactNode, useState } from 'react'
+import { destroyCookie } from 'nookies'
+import Router from 'next/router'
 
 
 //==Contexto=============================================================================
 type AuthContextData = {
     user: UserProps,
     isAuthenticated: boolean,
-    signIn: (credentials: SignInProps) => Promise<void>
+    signIn: (credentials: SignInProps) => Promise<void>,
+    signOut: () => void
 }
 
 type UserProps = {
@@ -21,6 +24,16 @@ type SignInProps = {
 
 export const AuthContext = createContext({} as AuthContextData);
 
+//==Logout===============================================================================
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@myCupcake.token');
+        Router.push('/');
+    } catch (err) {
+        console.log(`Erro: ${err} ao deslogar!`)
+    }
+}
+
 //==Provider do Contexto=================================================================
 type AuthProviderProps = {
     children: ReactNode
@@ -35,7 +48,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
     }
 
     return (
-        <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+        <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     )
